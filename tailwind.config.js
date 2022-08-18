4d3d15e0ed6ac9d412925fdc7278e0e41d8ece38
Rahmane Ndiaye <rahmane.ndiaye@mykritek.eu>
2022-08-18 22:16:47 +0000

    Add info to deploy in Heroku

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      padding: { "fluid-video": "56.25%" }
+    },
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+      adelia: ["ADELIA", "cursive"],
+      fredoka: ["Fredoka One", "serif"],
+      rubik: ["Silkscreen", 'serif']
+    },
+    container: {
+      center: true,
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
